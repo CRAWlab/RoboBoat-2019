0cc7e72a280348782725ac47aad750a18a0d1aec
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2019-04-20 18:32:15 -0500

    Working BlueRobotics T200 single thruster
    TODO:
    - 0.4 throttle is being interpreted as zero. Likely due to:
      + PWM timing in Adafruit library or
      + range of pulse width set
    - Account for thruster deadzone around zero throttle
    - Check limits on forward and reverse speeds

--- T200 Basic Control/T200_basic_control.py
+++ T200 Basic Control/T200_basic_control.py
@@ -28,7 +28,12 @@
 #   * 
 #
 # TODO:
-#   * 
+#   * 04/20/19 - JEV - joshua.vaughan@louisiana.edu
+#         - 0.4 throttle is being interpreted as zero. Likely due to:
+#           + PWM timing in Adafruit library or
+#           + range of pulse width set
+#         - Account for deadzone around zero throttle
+#
 ###############################################################################
 
 import time
@@ -36,14 +41,22 @@ import time
 from adafruit_servokit import ServoKit
 
 # Operation Constants
-ESC_STARTUP_DELAY = 3.0      # Time to sleep during initialization
+ESC_STARTUP_DELAY = 1.0         # Time to sleep during initialization
+MAX_THROTTLE = 50               # Maximum magnitude of throttle to use in test
+
+# TODO: 04/20/19 - JEV 
+#       * 0.4 throttle is being interpreted as zero. 
+#       * Likely due to:
+#           - PWM timing in Adafruit library or
+#           - range of pulse width set
+THROTTLE_OFFSET = 0.4           # Hack for now
 
 # The 16-channel PWM hat has (surprise!!) 16 channels
 kit = ServoKit(channels=16)
 
 # The ESC is connected to channel 0. We can map a name to that channel to make
 # the code easier to understand
-thruster = kit.servo[0]
+thruster = kit.continuous_servo[0]
 
 # The BlueRobotics ESC expects pulses in the range 1100-1900us, so we modify
 # the range of pulses for the pin connected to the ESC
@@ -51,64 +64,67 @@ thruster.set_pulse_width_range(1100, 1900)
 
 # To initialize the thruster, we need to send zero speed for some time
 # You should hear two beeps once the ESC is initialized
-thuster.throttle(0.0)
+print('Initializing...')
+thruster.throttle = 0.0 + THROTTLE_OFFSET
 time.sleep(ESC_STARTUP_DELAY)
+
 
 try:
     print('Starting positive throttle cycle.')
-    for speed in range(100):
+    for speed in range(MAX_THROTTLE):
         # The throttle command expects commands in the range -1 <= speed <= 1
         # So, we scale by 100
         throttle_command = speed/100
 
         print('  Current speed: {:+4d}% = {:+4.2f} command'.format(speed, throttle_command))
-        thruster.throttle(throttle_command)
-    
+        thruster.throttle = throttle_command + THROTTLE_OFFSET
+        
         # Then sleep a short time before looping
         time.sleep(0.1)
-    
-    for speed in range(100):
+
+    for speed in range(MAX_THROTTLE):
         # The throttle command expects commands in the range -1 <= speed <= 1
         # So, we scale by 100
-        speed = 100 - speed
+        speed = MAX_THROTTLE - speed
         throttle_command = speed/100
 
         print('  Current speed: {:+4d}% = {:+4.2f} command'.format(speed, throttle_command))
-        thruster.throttle(throttle_command)
-    
+        thruster.throttle = throttle_command + THROTTLE_OFFSET
+
         # Then sleep a short time before looping
         time.sleep(0.1)
 
 
     print('Starting negative throttle cycle.')
-    for speed in range(100):
+    for speed in range(MAX_THROTTLE):
         # The throttle command expects commands in the range -1 <= speed <= 1
         # So, we scale by 100
         throttle_command = -speed/100 # Just the negative of the loop above
 
         print('  Current speed: {:+4d}% = {:+4.2f} command'.format(-speed, throttle_command))
-        thruster.throttle(throttle_command)
-    
+        thruster.throttle = throttle_command + THROTTLE_OFFSET
+
         # Then sleep a short time before looping
         time.sleep(0.1)
 
-    for speed in range(100):
+    for speed in range(MAX_THROTTLE):
         # The throttle command expects commands in the range -1 <= speed <= 1
         # So, we scale by 100
-        speed = 100 - speed
+        speed = MAX_THROTTLE - speed
         throttle_command = -speed/100 # Just the negative of the loop above
 
         print('  Current speed: {:+4d}% = {:+4.2f} command'.format(-speed, throttle_command))
-        thruster.throttle(throttle_command)
+        thruster.throttle = throttle_command + THROTTLE_OFFSET
     
         # Then sleep a short time before looping
         time.sleep(0.1)
     
     # Thruster needs to be stopped
     print('Stopping...')
-    thruster.thottle(0.0)
+    thruster.throttle = 0.0 + THROTTLE_OFFSET
 
 finally:
+    print('Stopping...')
     # Stop the thruster on any exceptions or program exit
-    thruster.throttle(0.0)
+    thruster.throttle = 0.0 + THROTTLE_OFFSET
 
