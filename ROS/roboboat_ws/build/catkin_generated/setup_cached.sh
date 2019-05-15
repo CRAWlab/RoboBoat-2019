be96512250bcbd3bb1003a920f2c38a946b5c669
lxm7525 <49698124+lxm7525@users.noreply.github.com>
2019-05-15 12:24:50 -0500

    ROS workspace
    Added the ROS workspace with the joystick code we used for testing, added deadman button RT (untested)

---
+++ ROS/roboboat_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/luke/roboboat_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/luke/roboboat_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/luke/roboboat_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/luke/roboboat_ws/src:$ROS_PACKAGE_PATH"
