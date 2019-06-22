bbe09984e8c290fc31e496e77c9beeab6235535a
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2019-06-22 19:58:32 -0400

    Added status/LED publication to speed.py move_base script

--- Python Scripts/Navigation Scripts/speed.py
+++ Python Scripts/Navigation Scripts/speed.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+mode_pub = rospy.Publisher('/mode', String, queue_size=1, latch=True)
 
 waypoints = [  
     [(15, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0)],
@@ -32,15 +33,51 @@ def goal_pose(pose):
 
 
 if __name__ == '__main__':
-    rospy.init_node('patrol')
+    rospy.init_node('auto_speedtest')
+    
+    mode_pub = rospy.Publisher('/mode', String, queue_size=1, latch=True)
 
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)  
     client.wait_for_server()
 
-    while True:
-        for pose in waypoints:   
-            goal = goal_pose(pose)
-            client.send_goal(goal)
-            client.wait_for_result()
+    try:
+        # Publish the AUTONOMOUS message since we are going into autonomous mode
+        mode_pub.pulish('AUTONOMOUS')
+        rospy.loginfo('Going autonomous.')
+        
+        while True:
+            for pose in waypoints:   
+                goal = goal_pose(pose)
+                
+                rospy.loginfo('Sending goal {}'.format(pose))
+                client.send_goal(goal)
+
+                # Check the current state
+                # It should be
+                state = self.move_base.get_state()
+                
+                while state != GoalStatus.SUCCEEDED:
+                    # Wait one second
+                    rospy.sleep(1.0) 
+                    
+                    # I think we should sleep rather than the below. It seems
+                    # like it would end the pursuit of that goal
+                    # client.wait_for_result(rospy.Duration(1.0))
+                
+                    # Check the current state
+                    state = self.move_base.get_state()
+                    
+                
+                rospy.loginfo("Goal succeeded!")
+
+                
+    except (KeyboardInterrupt, SystemExit):
+        # If we get a keyboard interrupt cancel the goal request and go back
+        # to REMOTE on the light tower
+        rospy.loginfo('Cancelled goals.')
+        client.cancel_goal()
+        
+        rospy.logdebug('Switching back to REMOTE mode.')
+        mode_pub.pulish('REMOTE')
 
 
